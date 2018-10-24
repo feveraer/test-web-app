95093e0c2bf5b8c3f8330b49665324c3597f18bb
feveraer <frederic.everaert@gmail.com>
2018-10-24 13:08:34 +0200

    added home rest controller

---
+++ src/main/java/com/feveraer/testwebapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.feveraer.testwebapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "hello world!";
+    }
+}
