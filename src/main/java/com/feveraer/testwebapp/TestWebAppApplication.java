85b6db671b46357c3001ad01bf67e628391ff78a
feveraer <frederic.everaert@gmail.com>
2018-10-24 12:47:21 +0200

    spring boot initial setup

---
+++ src/main/java/com/feveraer/testwebapp/TestWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.feveraer.testwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWebAppApplication.class, args);
+	}
+}
