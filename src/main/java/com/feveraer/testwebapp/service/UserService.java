e41474faa76e14c3a6359ada8256ed7904ca699c
feveraer <frederic.everaert@gmail.com>
2018-10-25 11:40:09 +0200

    - updated UserService - previous commit added a mvn wrapper for reasons

--- src/main/java/com/feveraer/testwebapp/service/UserService.java
+++ src/main/java/com/feveraer/testwebapp/service/UserService.java
@@ -1,4 +1,11 @@
 package com.feveraer.testwebapp.service;
 
-public class UserService {
+import com.feveraer.testwebapp.dto.UserDto;
+
+import java.util.List;
+
+interface UserService {
+    UserDto getUserById(String id);
+    void saveUser(UserDto userDto);
+    List<UserDto> getAllUsers();
 }
